79087a27f62f56456944c0ffb42810e33066f807
elvina putri olivia <elvinavia123@gmail.com>
2025-08-20 19:23:33 +0700

    Info.jsx

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,5 +1,5 @@
 import { FaHtml5, FaCss3Alt, FaJs, FaGithub, FaGitAlt, FaReact, FaNodeJs } from "react-icons/fa";
-import { SiTailwindcss, SiNextdotjs, SiMongodb, SiVercel } from "react-icons/si";
+import { SiVercel } from "react-icons/si";
 
 export default function Info() {
   const skills = [
@@ -7,10 +7,7 @@ export default function Info() {
     { icon: <FaCss3Alt color="#1572B6" size="40" />, name: "CSS" },
     { icon: <FaJs color="#F7DF1E" size="40" />, name: "JavaScript" },
     { icon: <FaReact color="#61DAFB" size="40" />, name: "React" },
-    { icon: <SiTailwindcss color="#38BDF8" size="40" />, name: "Tailwind" },
-    { icon: <SiNextdotjs color="#fff" size="40" />, name: "Next.js" },
     { icon: <FaNodeJs color="#68A063" size="40" />, name: "Node.js" },
-    { icon: <SiMongodb color="#47A248" size="40" />, name: "MongoDB" },
     { icon: <FaGitAlt color="#F05032" size="40" />, name: "Git" },
     { icon: <FaGithub color="#181717" size="40" />, name: "GitHub" },
     { icon: <SiVercel color="#000000" size="40" />, name: "Vercel" },
